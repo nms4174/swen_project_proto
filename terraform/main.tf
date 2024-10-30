41633c7aea212b061a76fd81cd21d4545ddc40b6
nms4174 <nms4174@rit.edu>
2024-10-30 16:05:21 -0400

    init repo and terraform

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+resource "aws_amplify_app" "swen_project_proto" {
+  name       = "swen_project_proto"
+  repository = "https://github.com/nms4174/swen_project_proto"
+
+  # The default build_spec added by the Amplify Console for React.
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - yarn install
+        build:
+          commands:
+            - yarn run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+}
